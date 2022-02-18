9c793d8c1b786c62ad99661d48240088b32d606a
Zoran Đukić <zorandjukic.os@gmail.com>
2022-02-18 12:49:55 +0100

    Finished implementing API.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,20 +1,31 @@
 import React from "react";
 
-let accessToken = {
+const clientId = '571f813af943473099cc536ae4c759a3';
+const redirectUri = 'http://localhost:3000';
+let accessToken;
 
-}
 
 const Spotify = {
-    getAccessToken(){
-        if(accessToken){
+    getAccessToken() {
+        if (accessToken) {
             return accessToken;
         }
 
-        const accessToken = window.location.href.match(/access_token=([^&]*)/);
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = windows.location.href.match(/expires_in=([^&]*)/);
 
-    }
-}
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            windows.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }else{
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}
+            windows.location = accessUrl;
 
+
+    }       
+}
 
 export default Spotify;
